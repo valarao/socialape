b642e6200fa7461803af5d3a01680d2c6e4344c1
Miguel Valarao <miguel.valarao@gmail.com>
2020-11-20 14:42:39 -0800

    Authentication Middleware (#6)

--- functions/index.js
+++ functions/index.js
@@ -20,6 +20,7 @@ admin.initializeApp(firebaseConfig);
 
 const db = admin.firestore();
 
+// Retrieve screams
 app.get('/screams', async (request, response) => {
   try {
     const docs = await db
@@ -45,11 +46,37 @@ app.get('/screams', async (request, response) => {
   }
 });
 
-app.post('/scream', async (request, response) => {
+const firebaseAuth = async (request, response, next) => {
+  try {
+    let idToken;
+    if (request.headers.authorization && request.headers.authorization.startsWith('Bearer ')) {
+      idToken = request.headers.authorization.split('Bearer ')[1];
+    } else {
+      return response.status(403).json({ error: 'Unauthorized' });
+    }
+  
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    request.user = decodedToken;
+    
+    const firebaseUserData = await db.collection('users')
+      .where('userId', '==', request.user.uid)
+      .limit(1)
+      .get();
+    
+    request.user.handle = firebaseUserData.docs[0].data().handle;
+    return next();
+  } catch (error) {
+      console.error('Error while verifying token ', error);
+      return response.status(403).json(error);
+  }
+}
+
+// Post one scream
+app.post('/scream', firebaseAuth, async (request, response) => {
   try {
     const newScream = {
       body: request.body.body,
-      userHandle: request.body.userHandle,
+      userHandle: request.user.handle,
       createdAt: new Date().toISOString(),
     };
     const newDoc = await db.collection('screams').add(newScream);
