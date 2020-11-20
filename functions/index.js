bfc34a167a078ecfd1d1517d38a63e86a1ebbb26
Miguel Valarao <miguel.valarao@gmail.com>
2020-11-19 23:39:48 -0800

    Fix express (#2)

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,25 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
-
-exports.getScreams = functions.https.onRequest(async (request, response) => {
+app.get('/screams', async (request, response) => {
   try {
-    const docs = await admin.firestore().collection('screams').get();
+    const docs = await admin
+      .firestore()
+      .collection('screams')
+      .orderBy('createdAt', 'desc')
+      .get();
+
     const screams = [];
     docs.forEach((doc) => {
-      screams.push(doc);
+      screams.push({
+        screamId: doc.id,
+        body: doc.data().body,
+        userHandle: doc.data().body,
+        createdAt: doc.data().createdAt,
+      });
     });
     return response.json(screams);
   } catch (error) {
@@ -27,18 +30,12 @@ exports.getScreams = functions.https.onRequest(async (request, response) => {
   }
 });
 
-exports.createScream = functions.https.onRequest(async (request, response) => {
+app.post('/scream', async (request, response) => {
   try {
-    if (request.method !== 'POST') {
-      return response.status(400).json({
-        error: 'Method not allowed.',
-      });
-    }
-
     const newScream = {
       body: request.body.body,
       userHandle: request.body.userHandle,
-      createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+      createdAt: new Date().toISOString(),
     };
     const newDoc = await admin.firestore().collection('screams').add(newScream);
     return response.json({
@@ -51,3 +48,5 @@ exports.createScream = functions.https.onRequest(async (request, response) => {
     });
   }
 });
+
+exports.api = functions.https.onRequest(app);
