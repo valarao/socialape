1e70af198cb2803545aadc012cb1ba75280fb3c5
Miguel Valarao <miguel.valarao@gmail.com>
2020-11-20 16:06:03 -0800

    Image Upload

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,5 +1,10 @@
 const firebase = require('firebase');
-const { db } = require('../util/admin');
+const Busboy = require('busboy');
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
+const { admin, db } = require('../util/admin');
 const config = require('../util/config');
 const { validateSignupData, validateLoginData } = require('../util/validators');
 
@@ -21,6 +26,8 @@ exports.signup = async (request, response) => {
       return response.status(400).json(errors);
     }
 
+    const noImg = 'no-img.png';
+
     // Check if handle already is taken
     const user = await db.doc(`/users/${newUser.handle}`).get();
     if (user.exists) {
@@ -40,6 +47,7 @@ exports.signup = async (request, response) => {
       handle: newUser.handle,
       email: newUser.email,
       createdAt: new Date().toISOString(),
+      imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
       userId,
     };
 
@@ -52,9 +60,9 @@ exports.signup = async (request, response) => {
         error: 'Email is already in use',
       });
     }
-    return response.status(500).json({error: error.code});
+    return response.status(500).json({ error: error.code });
   }
-}
+};
 
 exports.login = async (request, response) => {
   try {
@@ -77,9 +85,61 @@ exports.login = async (request, response) => {
   } catch (error) {
     console.log(error);
     if (error.code === 'auth/wrong-password') {
-      return response.status(403).json({general: 'Wrong credentials, please try again'});
+      return response
+        .status(403)
+        .json({ general: 'Wrong credentials, please try again' });
     }
-    
-    return response.status(500).json({error: error.code});
+
+    return response.status(500).json({ error: error.code });
   }
-}
+};
+
+exports.uploadImage = async (request, response) => {
+  try {
+    const busboy = new Busboy({ headers: request.headers });
+
+    let imageFilename;
+    let imageToBeUploaded = {};
+
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+      if (mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
+        return response.status(400).json({ error: 'Wrong file type submitted' });
+      }
+      const imageExtension = filename.split('.')[
+        filename.split('.').length - 1
+      ];
+      imageFilename = `${Math.round(
+        Math.random() * 10000000000000,
+      )}.${imageExtension}`;
+      const filepath = path.join(os.tmpdir(), imageFilename);
+      imageToBeUploaded = { filepath, mimetype };
+      file.pipe(fs.createWriteStream(filepath));
+    });
+
+    busboy.on('finish', async () => {
+      await admin
+        .storage()
+        .bucket()
+        .upload(imageToBeUploaded.filepath, {
+          resumable: false,
+          metadata: {
+            metadata: {
+              contentType: imageToBeUploaded.mimetype,
+            },
+          },
+        });
+
+      const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFilename}?alt=media`;
+      await db.doc(`/users/${request.user.handle}`).update({ imageUrl });
+    });
+
+    busboy.end(request.rawBody);
+
+    return response
+      .status(201)
+      .json({ message: 'Image uploaded successfully' });
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ error: error.code });
+  }
+};
