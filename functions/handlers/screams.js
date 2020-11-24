0b050e20aaffc111c32653280ab9dea25acf64ad
Miguel Valarao <miguel.valarao@gmail.com>
2020-11-23 21:14:56 -0800

    Get and Post Comment (#10)
    * Get and Post Comment
    
    * Fix return

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express');
 const { db } = require('../util/admin');
 
 exports.getAllScreams = async (request, response) => {
@@ -36,6 +37,64 @@ exports.postOneScream = async (request, response) => {
     return response.status(201).json({
       message: `Document ${newDoc.id} created successfully`,
     });
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({
+      error,
+    });
+  }
+};
+
+// Get one scream
+exports.getScream = async (request, response) => {
+  try {
+    const screamDoc = await db.doc(`/screams/${request.params.screamId}`).get();
+    if (!screamDoc.exists) {
+      return response.status(404).json({ error: 'Scream not found' });
+    }
+    const screamData = screamDoc.data();
+    screamData.screamId = screamDoc.id;
+
+    const commentsDocs = await db
+      .collection('comments')
+      .orderBy('createdAt', 'desc')
+      .where('screamId', '==', request.params.screamId)
+      .get();
+    screamData.comments = [];
+    commentsDocs.forEach((commentDoc) => {
+      screamData.comments.push(commentDoc.data());
+    });
+    return response.status(200).json(screamData);
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({
+      error,
+    });
+  }
+};
+
+// Comment on a scream
+exports.commentOnScream = async (request, response) => {
+  try {
+    if (request.body.body.trim() === '') {
+      return response.status(400).json({ error: 'Must not be empty' });
+    }
+
+    const newComment = {
+      body: request.body.body,
+      createdAt: new Date().toISOString(),
+      screamId: request.params.screamId,
+      userHandle: request.user.handle,
+      userImage: request.user.imageUrl,
+    };
+
+    const screamsDoc = await db.doc(`/screams/${request.params.screamId}`).get();
+    if (!screamsDoc.exists) {
+      return response.status(404).json({ error: 'Scream not found' });
+    }
+
+    await db.collection('comments').add(newComment);
+    return response.json(newComment);
   } catch (error) {
     console.log(error);
     return response.status(500).json({
