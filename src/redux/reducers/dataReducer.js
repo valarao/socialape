e7508d8fa4e2635e1bc7c0a751a273fd9bc8709a
Miguel Valarao <miguel.valarao@gmail.com>
2020-12-06 19:18:29 -0800

    Delete Button

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -1,10 +1,16 @@
-import { SET_SCREAMS, LIKE_SCREAM, UNLIKE_SCREAM, LOADING_DATA } from '../types';
+import {
+  SET_SCREAMS,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM,
+  LOADING_DATA,
+  DELETE_SCREAM,
+} from '../types';
 
 const initialState = {
   screams: [],
   scream: {},
   loading: false,
-}
+};
 
 const dataReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -12,24 +18,33 @@ const dataReducer = (state = initialState, action) => {
       return {
         ...state,
         loading: true,
-      }
+      };
     case SET_SCREAMS:
       return {
         ...state,
         screams: action.payload,
         loading: false,
+      };
+    case LIKE_SCREAM:
+    case UNLIKE_SCREAM:
+      let index = state.screams.findIndex(
+        (scream) => scream.screamId === action.payload.screamId,
+      );
+      state.screams[index] = action.payload;
+      return {
+        ...state,
+      };
+    case DELETE_SCREAM:
+      index = state.screams.findIndex(
+        (scream) => scream.screamId === action.payload,
+      );
+      state.screams.splice(index, 1);
+      return {
+        ...state,
       }
-      case LIKE_SCREAM:
-      case UNLIKE_SCREAM:
-        let index = state.screams.findIndex((scream) => scream.screamId === action.payload.screamId);
-        state.screams[index] = action.payload;
-        return {
-          ...state,
-
-        }
     default:
       return state;
   }
-}
+};
 
 export default dataReducer;
