72fb5cdad4f195d343c80a0d680781e98b0e4857
Miguel Valarao <miguel.valarao@gmail.com>
2020-12-06 18:12:50 -0800

    Like Unlike Actions (#24)

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import PropTypes from 'prop-types';
+
+
+import { connect } from 'react-redux';
 
 import withStyles from '@material-ui/core/styles/withStyles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+
+import { likeScream, unlikeScream } from '../redux/actions/dataActions';
 
 const styles = {
   card: {
@@ -52,4 +58,21 @@ class Scream extends Component {
   }
 }
 
-export default withStyles(styles)(Scream);
+Scream.propTypes = {
+  likeScream: PropTypes.func.isRequired,
+  unlikeScream: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
+  scream: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+const mapActionsToProps = {
+  likeScream,
+  unlikeScream,
+};
+
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Scream));
--- src/redux/types.js
+++ src/redux/types.js
@@ -9,5 +9,11 @@ export const SET_IMAGE = 'SET_IMAGE';
 export const SET_ERRORS = 'SET_ERRORS';
 export const LOADING_UI = 'LOADING_UI';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const LOADING_DATA = 'LOADING_DATA';
+
 
 // Data Reducer Types
+export const SET_SCREAMS = 'SET_SCREAMS';
+export const SET_SCREAM = 'SET_SCREAM';
+export const LIKE_SCREAM = 'LIKE_SCREAM';
+export const UNLIKE_SCREAM = 'UNLIKE_SCREAM';
